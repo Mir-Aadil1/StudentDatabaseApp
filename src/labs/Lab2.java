9539beb8ca020729c291c129bca51f73ea48d9fe
Mir-Aadil1 <ermiraadil@gmail.com>
2024-10-25 17:51:29 +0530

    Added all methods

--- src/labs/Lab2.java
+++ src/labs/Lab2.java
@@ -4,6 +4,15 @@ public class Lab2 {
     public static void main(String[] args) {
         Student student1 = new Student("Aadil", "321456987");
         Student student2 = new Student("Aditya", "321434987");
+        student1.enroll("Math101");
+        student1.enroll("Eng101");
+        student1.enroll("His101");
+
+        //student1.showCourse();
+        //student1.checkBalance();
+        student1.payTution(600);
+        student1.checkBalance();
+        System.out.println(student1.toString());
 
     }
 }
@@ -14,8 +23,17 @@ class Student {
     private static int iD=1000;
     private String userId;
     private String name ;
+    private String phone;
+    private String city;
+    private String state;
     private String ssn;
     private String email;
+    public String courses = "";
+    private int balance = 0;
+
+    private static final int costOfCourse = 800;
+
+
 
 
     public Student (String name, String ssn) {
@@ -25,6 +43,39 @@ class Student {
         setUserId();
         setEmail();
 
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 
     private void setEmail() {
@@ -41,5 +92,42 @@ class Student {
         userId = iD + "" + randNum + ssn.substring(6);
         System.out.println("Your User ID: " + userId);
     }
+
+    public void enroll (String course) {
+
+        this.courses = this.courses + "\n" + course;
+        balance = balance + costOfCourse;
+
+
+
+    }
+
+    public void payTution( int amount) {
+        System.out.println("Payment: " + amount);
+        balance = balance -amount;
+
+
+
+    }
+
+    public void checkBalance(){
+        System.out.println("Balance $" + balance);
+    }
+
+    public void showCourse(){
+        System.out.println(courses);
+
+
+    }
+    @Override
+    public String toString(){
+        return "[Name: " + name + "]\n[COURSES: " + courses + "]" + "\n[Balance:" + balance + "]\n";
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
 
 }
